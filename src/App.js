2351cd54c7076add51cb8680984ceee51ed4f2d6
Dave Herzog <davidjherzog@gmail.com>
2021-01-24 14:55:40 -0500

    media queries with radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 const app = props => {
@@ -89,12 +89,14 @@ const app = props => {
   }
 
   return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p className={classes.join(' ')}>This is really working</p>
-      <button style={style} onClick={() => togglePersonsHandler()}>Toggle Persons</button>
-      {persons}
-    </div>
+    <StyleRoot>
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working</p>
+        <button style={style} onClick={() => togglePersonsHandler()}>Toggle Persons</button>
+        {persons}
+      </div>
+    </StyleRoot>
   );
 }
 
